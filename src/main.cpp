37f760a47727edb9effe10e114fc9d896b6f50dd
SonTV <kingson.a12@gmail.com>
2020-04-19 16:52:47 +0700

    - Stop logging to terminal

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,26 @@
 #include <string>
 
 using namespace std;
-
 int main(int argc, char **argv)
 {
-	string time = "00_";
+	string time = "joint";
 	google::InitGoogleLogging(argv[0]);
 	google::SetLogDestination(google::GLOG_INFO, time.c_str());
-	google::SetLogFilenameExtension(".txt");
+	google::SetStderrLogging(google::NUM_SEVERITIES);
+	google::SetLogFilenameExtension(".log_");
+	
 	int count = 0;
 	while (1)
 	{
-		if (count++ % 100 == 0)
+		if (count++ % 20 == 0)
 		{
-			time = to_string(count-1);
-			google::SetLogDestination(google::GLOG_INFO, time.c_str());
+			google::SetLogDestination(google::GLOG_INFO, (time + to_string(count)).c_str());
 		}
 		LOG(ERROR)<<"ERROR Hello world";
 		LOG(INFO)<<"INFO Hello world";
 		LOG(WARNING)<<"WARNING Hello world";
 		usleep(10000);
-		if (count >= 1000)
+		if (count >= 100)
 		{
 			break;
 		}
